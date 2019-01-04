0bb45f1a784510e0969924ca710438bf4c343d8b
wanda belingga putra <45660041+WandaBelinggaPutra@users.noreply.github.com>
2019-01-04 20:56:59 +0700

    Add files via upload

---
+++ Menghitung Jarak Tempuh.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+int s,v,t;
+
+printf("Masukkan kecepatan (v) : ");scanf("%i",&v);
+printf("Masukkan waktu dalam sekon (t) : ");scanf("%i",&t);
+s=v*t;
+printf("S = %i * %i = %i Meter",v,t,s);
+getch();
+}
+
